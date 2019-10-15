9df5e81c2e7aeb80e6229f3bccc0b228b1a41648
dnagafonov <denisagafonovwork@gmail.com>
2019-10-15 22:39:11 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Table from "./components/list/table/table";
+import SearchPanel from "./components/list/add-peron-pannel";
 
 export default class App extends React.Component{
     render() {
         return (
-            <div>
-
+            <div style={{display: "inline"}}>
+                <Table/>
+                <SearchPanel/>
             </div>
         );
     }
--- src/components/list/add-peron-pannel/index.js
+++ src/components/list/add-peron-pannel/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 export default class SearchPanel extends React.Component{
+    state = {
+        text: "standard text"
+    }
+    onAddButtonClicked = () => {
+        console.log("SOSI", this.addInformation.value)
+        this.setState({text: this.addInformation})
+    }
     render() {
         return(
             <div>
-                <input placeholder="Новый элемент" type = "text"/>
-                <button>addEl</button>
+                <input placeholder="Новый элемент" type = "text" ref={el => this.addInformation = el}/>
+                    <button onClick={this.onAddButtonClicked.bind(this)}>addEl</button>
             </div>
         )
     }
